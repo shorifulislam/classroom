0cb5ddc6b70198de6824be8c36013225847ffea8
Pierre Merlin <pierrem@apido.org>
2016-12-15 15:14:36 +0100

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161214140212) do
 
-  create_table "students", force: :cascade do |t|
+  create_table "rooms", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "rooms", force: :cascade do |t|
+  create_table "students", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

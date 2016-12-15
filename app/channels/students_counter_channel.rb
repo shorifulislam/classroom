2ba931e8fb636ea25920818629556e3512cc1f00
amaury <vanespen.amaury@gmail.com>
2016-12-15 17:22:30 +0100

    working console log

--- app/channels/students_counter_channel.rb
+++ app/channels/students_counter_channel.rb
@@ -1,10 +1,34 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class StudentsCounterChannel < ApplicationCable::Channel
+
+  @@STUDENTS_COUNTER = 0
   def subscribed
     # stream_from "some_channel"
+
+
+    #stream_from "StudentsCounterChannel"
+    stream_from "some_channel"
+    #increment counter
+    @@STUDENTS_COUNTER += 1
+
+    #broadcast (to move where it is called)
+    broadcast_new_counter
+
+
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+
+    #decrement
+    @@STUDENTS_COUNTER -= 1
+    broadcast_new_counter
+
+  end
+
+  private
+
+  def broadcast_new_counter
+    ActionCable.server.broadcast "some_channel", @@STUDENTS_COUNTER
   end
 end
